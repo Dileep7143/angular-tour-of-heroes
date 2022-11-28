4218ae3b07543665bc1e09c5456513c9e12cb2d3
Dileep7143 <ampoludileep7@gmail.com>
2022-11-28 14:24:41 +0530

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Kumar' },
+  { id: 2, name: 'Sai' },
+  { id: 3, name: 'Gyanesh' },
+  { id: 4, name: 'Sunil' },
+  { id: 5, name: 'Dinesh' },
+  { id: 6, name: 'Deva' },
+  { id: 7, name: 'Siva' },
+  { id: 8, name: 'Dileep' },
+  { id: 9, name: 'Khalid' }
+];
